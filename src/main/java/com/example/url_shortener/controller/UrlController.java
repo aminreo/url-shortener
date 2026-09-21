7e1051708ec5afc49c01e47549d9e136db7b5908
Amin <ayari.am@campus.lmu.de>
2026-09-21 16:49:24 +0200

    added GET routing

--- src/main/java/com/example/url_shortener/controller/UrlController.java
+++ src/main/java/com/example/url_shortener/controller/UrlController.java
@@ -2,6 +2,7 @@ package com.example.url_shortener.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,6 +13,7 @@ import com.example.url_shortener.service.UrlService.UrlRequest;
 import com.example.url_shortener.service.UrlService.UrlResponse;
 
 import jakarta.validation.Valid;
+
 
 @RestController
 public class UrlController {
@@ -32,6 +34,12 @@ public class UrlController {
     public UrlResponse urlRequestMap( @Valid  @RequestBody UrlRequest req){
             return urlService.createUrl(req);
     }
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/links/{code}")
+    public String getLink( @PathVariable String code) {
+        return urlService.findUrl(code);
+    }
+    
     
 
 }
